bdd683b278bb19cdfb538f3da6276b25ddf007d4
varchunn <varvaram186@gmail.com>
2025-05-01 17:20:05 +0300

    Initial Commit

---
+++ Strategy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Strategy
+//
+//  Created by Варя Мінєнкова on 01.05.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
